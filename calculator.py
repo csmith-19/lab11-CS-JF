6b701963d94e9274bf0147f45177d43413be156b
csmith-19 <csmith19@ufl.edu>
2026-08-01 15:27:39 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -15,26 +15,28 @@ def hypotenuse(a, b):
     return math.hypot(a, b)
 
 # First example
-def add(a, b): 
+
+def add(a, b):
     return a + b
+
 
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError
-    return b / a
+    else:
+        return b / a
 
 def logarithm(a, b):
     if a <= 0:
         raise ValueError
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
-
 
